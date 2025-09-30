1673d41d8ebd7edd4d53a89ea8960d37d74669c0
Runbinlin <910073330@qq.com>
2025-09-30 05:36:48 +0000

    重构：更新 Android 项目结构和构建配置
    1. 更新了 Android Gradle 插件到 8.1.2
    2. 更新了 Kotlin 版本到 1.9.0
    3. 配置了 AndroidX 支持
    4. 更新了项目结构以遵循最佳实践
    5. 添加了必要的资源文件
    6. 迁移了现有代码到新的项目结构

--- android/app/src/main/java/com/captiveconnect/app/MainActivity.kt
+++ android/app/src/main/java/com/captiveconnect/app/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.captiveconnect.app
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
+import com.captiveconnect.app.service.HotspotService
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.System.canWrite(this)) {
+            startActivity(Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS).apply {
+                data = android.net.Uri.parse("package:$packageName")
+            })
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        restartHotspotService()
+    }
+
+    private fun restartHotspotService() {
+        Intent(this, HotspotService::class.java).also {
+            it.action = HotspotService.ACTION_STOP
+            startService(it)
+            it.action = HotspotService.ACTION_START
+            startService(it)
+        }
     }
 }
